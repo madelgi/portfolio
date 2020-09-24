853c8e0a8d834eda6590e8dfb11252ec9173376b
Max Del Giudice <maxdelgiudice@gmail.com>
2020-09-24 02:37:38 +0000

    Various tweeks to orchestration files, get ceritificate/nginx working

--- frontend/bin/run.sh
+++ frontend/bin/run.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
+
 if [ "$BUILD_ENV" == "development" ]; then
+    echo "Running development server"
     gatsby develop -H 0.0.0.0
 else
+    echo "Running production server"
+    gatsby build
     gatsby serve -H 0.0.0.0
 fi
